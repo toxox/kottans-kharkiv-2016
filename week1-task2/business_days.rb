d77f8b03a4e999fba4f49f4dc823fef8b9cb594b
Anton Trublin <anton.trublin@gmail.com>
2016-11-07 13:01:15 +0200

    Complete BusinessDays class.

--- week1-task2/business_days.rb
+++ week1-task2/business_days.rb
@@ -8,14 +8,28 @@ class BusinessDays
     @start = args[:start]
     @region = args[:region]
 
-    business_days
+    @business_days = business_days(@start, @region)
   end
 
-  def business_days
-    date = Date.civil(@start.year, @start.month, @start.day)
+  def each(&block)
+    @business_days.each(&block)
+    self
+  end
 
-    p Holidays.on(date, :fr)
+  def business_days(start, region)
+    result = []
+    start_date = Date.civil(start.year, start.month, start.day)
+    end_date = Date.today
+
+    while start_date <= end_date
+      unless start_date.wday == 6 || start_date.wday == 0
+        result << start_date if Holidays.on(start_date, region).empty?
+      end
+      start_date += 1
+    end
+
+    result
   end
 end
 
-BusinessDays.new(start: Date.parse('2016-05-01'), region: :fr)
+p BusinessDays.new(start: Date.parse('2016-05-01'), region: :fr).reject { |d| d.wday == 5 }.take(5)
